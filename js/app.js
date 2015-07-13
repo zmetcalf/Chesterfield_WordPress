32d927c12e4fecfed1c618f4c59059f32883ba3a
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-07-12 20:47:28 -0600

    Moved over template files from theme

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 $(document).foundation();
+
+$(function() {
+
+  // Vegas Slideshow
+
+  var slides = [];
+
+  $.each($('.background_images'), function (index, image) {
+    slides.push({ src: $(image).attr('data-image') });
+  });
+
+  $('body').vegas({
+    slides: slides,
+    timer: false
+  });
+
+  // Masonry
+
+  $('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 0,
+  });
+});

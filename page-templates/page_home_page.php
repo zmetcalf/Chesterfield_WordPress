80383688e7c0cda202aaa283cca6ff8ce052d097
Zachery Metcalf <zachery.metcalf@gmail.com>
2016-01-07 20:31:57 -0700

    Change title on home page

--- page-templates/page_home_page.php
+++ page-templates/page_home_page.php
@@ -6,7 +6,7 @@
 <div class="home_callout row">
   <div class="large-8 medium-8 small-10 small-offset-1 medium-offset-2 large-offset-2">
     <div class="panel radius">
-      <h1><?php the_title(); ?></h1>
+      <h1><?php get_post_meta( $post->ID, 'callout_title', TRUE ) ?></h1>
 		  <?php while ( have_posts() ) : the_post(); ?>
 
         <?php the_content(); ?>
